65c311909a363f859e6c871205cb452f5e632f8a
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-03-04 16:36:57 +0900

    ABC086C回答完了

--- Beginners Selection/ABC086C_Traveling.py
+++ Beginners Selection/ABC086C_Traveling.py
@@ -3,6 +3,24 @@
 # https://atcoder.jp/contests/abs/tasks/arc089_a
 
 # 入力
-
+N = int(input())
+t = [0]
+x = [0]
+y = [0]
+for _ in range(N):
+    ti, xi, yi = map(int, input().split())
+    t.append(ti)
+    x.append(xi)
+    y.append(yi)
 
 # 出力
+for i in range(N):
+    dist = abs(x[i+1] - x[i]) + abs(y[i+1] - y[i])
+    delta = t[i+1] - t[i]
+    if dist > delta:
+        print("No")
+        exit()
+    if (delta - dist) % 2 != 0:
+        print("No")
+        exit()
+print("Yes")
