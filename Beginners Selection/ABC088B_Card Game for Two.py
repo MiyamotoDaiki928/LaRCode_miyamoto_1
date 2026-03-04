b44a535e2635089bebe09c3bfe488721b7e761aa
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-03-04 16:35:28 +0900

    ABC088B回答完了

--- Beginners Selection/ABC088B_Card Game for Two.py
+++ Beginners Selection/ABC088B_Card Game for Two.py
@@ -3,6 +3,13 @@
 # https://atcoder.jp/contests/abs/tasks/abc088_b
 
 # 入力
-
+n = int(input())
+ai = list(map(int, input().split()))
+ai.sort(reverse = True)
 
 # 出力
+alice_ai = [ai[i] for i in range(len(ai)) if i % 2 == 0]
+bob_ai = [ai[i] for i in range(len(ai)) if i % 2 != 0]
+alice = sum(alice_ai)
+bob = sum(bob_ai)
+print(alice - bob)
