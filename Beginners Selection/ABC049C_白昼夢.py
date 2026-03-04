e1b1c33e0561a7820f73f5197f53aa0d4dff6a43
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-03-04 16:36:42 +0900

    ABC049C回答完了

--- Beginners Selection/ABC049C_白昼夢.py
+++ Beginners Selection/ABC049C_白昼夢.py
@@ -3,6 +3,21 @@
 # https://atcoder.jp/contests/abs/tasks/arc065_a
 
 # 入力
-
+s = input()
 
 # 出力
+s = s[::-1]
+patterns = ["dream", "dreamer", "erase", "eraser"]
+patterns = [p[::-1] for p in patterns]
+i = 0
+while i < len(s):
+    check = False
+    for p in patterns:
+        if s[i:i+len(p)] == p:
+            i += len(p)
+            check = True
+            break
+    if not check:
+        print("NO")
+        exit()
+print("YES")
