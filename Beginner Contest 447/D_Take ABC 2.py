cfb312516dc52cf7b68299781ee73500dc6849ca
miyamoto <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-03-23 23:46:04 +0900

    D問題回答完了

--- Beginner Contest 447/D_Take ABC 2.py
+++ Beginner Contest 447/D_Take ABC 2.py
@@ -1,3 +1,20 @@
 # 2026-02-28(土) 21:00 ~ 2026-02-28(土) 22:40 (100分)
 # AtCoder Beginner Contest 447
 # https://atcoder.jp/contests/abc447/tasks/abc447_d
+
+s = input()
+a = 0
+ab = 0
+ans = 0
+for c in s:
+    if c == "A":
+        a += 1
+    elif c == "B":
+        if a > 0:
+            ab += 1
+            a -= 1
+    elif c == "C":
+        if ab > 0:
+            ab -= 1
+            ans += 1
+print(ans)
