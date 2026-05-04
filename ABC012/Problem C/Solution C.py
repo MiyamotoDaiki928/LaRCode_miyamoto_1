898f7f9ab03b7fdc45046f8163c87726f2775505
MiyamotoDaiki928 <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-05-04 12:34:27 +0900

    C問題回答完了

--- ABC012/Problem C/Solution C.py
+++ ABC012/Problem C/Solution C.py
@@ -6,4 +6,9 @@
 # コードテストはTesterファイルをターミナルで実行してください
 # =======================================================
 
+n = int(input())
 
+left = 2025 - n
+for i in range(1, 10):
+    if left % i == 0 and left / i < 10:
+        print(str(i) + " x " + str(int(left / i)))
