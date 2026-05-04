0546523b73bb5626d9b2d89d10a06f95fd7a38c1
MiyamotoDaiki928 <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-05-04 13:56:19 +0900

    D問題回答完了

--- ABC012/Problem D/Solution D.py
+++ ABC012/Problem D/Solution D.py
@@ -6,4 +6,30 @@
 # コードテストはTesterファイルをターミナルで実行してください
 # =======================================================
 
+n, m = map(int, input().split())
 
+INF = 10**15
+
+dist = [[INF]*n for _ in range(n)]
+for i in range(n):
+    dist[i][i] = 0
+
+for _ in range(m):
+    a, b, t = map(int, input().split())
+    a -= 1
+    b -= 1
+    dist[a][b] = t
+    dist[b][a] = t
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+
+ans = INF
+for i in range(n):
+    worst = max(dist[i])
+    ans = min(ans, worst)
+
+print(ans)
