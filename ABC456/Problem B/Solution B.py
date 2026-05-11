3bb8108810da33ed71308a8b73aa4090db7b8f8c
MiyamotoDaiki928 <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-05-11 14:31:26 +0900

    B問題回答完了

--- ABC456/Problem B/Solution B.py
+++ ABC456/Problem B/Solution B.py
@@ -6,4 +6,15 @@
 # コードテストはTesterファイルをターミナルで実行してください
 # =======================================================
 
+A = [list(map(int, input().split())) for _ in range(3)]
 
+count = 0
+for i in range(6):
+    for j in range(6):
+        for k in range(6):
+            vals = [A[0][i], A[1][j], A[2][k]]
+            
+            if sorted(vals) == [4, 5, 6]:
+                count += 1
+
+print(count / 216)
