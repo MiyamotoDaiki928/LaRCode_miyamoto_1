2035f617676b19fde00dc4b6c4ad1b7effd7d101
MiyamotoDaiki928 <daiki.miyamoto.4i@stu.hosei.ac.jp>
2026-05-11 16:17:29 +0900

    D問題回答完了

--- ABC457/Problem D/Solution D.py
+++ ABC457/Problem D/Solution D.py
@@ -7,3 +7,26 @@
 # =======================================================
 
 
+n, k = map(int, input().split())
+a = [0] + list(map(int, input().split()))
+
+def isok(x):
+    nk = 0
+    for i in range(1, n + 1):
+        if a[i] < x:
+            nk += (x - a[i] + i - 1) // i
+            if nk > k:
+                return False
+    return True
+
+ok = 1
+ng = a[1] + k + 1
+
+while ng - ok > 1:
+    m = (ok + ng) // 2
+    if isok(m):
+        ok = m
+    else:
+        ng = m
+
+print(ok)
